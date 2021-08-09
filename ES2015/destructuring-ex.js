c57f53df915a6b9909a66bf87c1c21578c604393
chrisstorrer <storrer.chris@gmail.com>
2021-08-08 19:00:32 -0700

    Add destructuring ex. solutions

---
+++ ES2015/destructuring-ex.js
@@ -0,0 +1,110 @@
+let facts = {numPlanets: 8, yearNeptuneDiscovered: 1846};
+let {numPlanets, yearNeptuneDiscovered} = facts;
+
+console.log(numPlanets); 
+// 8
+console.log(yearNeptuneDiscovered); 
+// 1846
+
+let planetFacts = {
+    numPlanets: 8,
+    yearNeptuneDiscovered: 1846,
+    yearMarsDiscovered: 1659
+};
+  
+let {numPlanets, ...discoveryYears} = planetFacts;
+  
+console.log(discoveryYears); // {yearNeptuneDiscovered: 1846, yearMarsDiscovered: 1659}
+
+function getUserData({firstName, favoriteColor="green"}){
+    return `Your name is ${firstName} and you like ${favoriteColor}`;
+}
+  
+getUserData({firstName: "Alejandro", favoriteColor: "purple"})  // Your name is Alejandro and you like purple
+  
+getUserData({firstName: "Melissa"})  // Your name is Melissa and you like green
+  
+getUserData({})  // Your name is undefined and you like green
+
+let [first, second, third] = ["Maya", "Marisa", "Chi"];
+
+console.log(first); 
+// Maya
+console.log(second); 
+// Marisa
+console.log(third); 
+// Chi
+
+let [raindrops, whiskers, ...aFewOfMyFavoriteThings] = [
+    "Raindrops on roses",
+    "whiskers on kittens",
+    "Bright copper kettles",
+    "warm woolen mittens",
+    "Brown paper packages tied up with strings"
+];
+  
+console.log(raindrops); 
+// "Raindrops on roses"
+console.log(whiskers); 
+// "whiskers on kittens"
+console.log(aFewOfMyFavoriteThings);
+  
+// ["Bright copper kettles", "warm woolen mittens", "Brown paper packages tied up with strings"]
+
+// Array Destructuring 3
+// What does the following code return/print?
+
+let numbers = [10, 20, 30];
+// just swapping numbers around
+[numbers[1], numbers[2]] = [numbers[2], numbers[1]];
+
+console.log(numbers);
+// [10, 30, 20];
+
+
+
+
+const obj = {
+  numbers: {
+    a: 1,
+    b: 2,
+  }
+};
+
+const { a, b } = obj.numbers;
+
+// ES2015 One-Line Array Swap with Destructuring
+
+[arr[0], arr[1]] = [arr[1], arr[0]];
+
+// raceResults()
+// Write a function called raceResults which accepts a single array argument. It should return an object with the keys first, second, third, and rest.
+
+// first: the first element in the array
+// second: the second element in the array
+// third: the third element in the array
+// rest: all other elements in the array
+
+// Write a one line function to make this work using
+// An arrow function
+// Destructuring
+// ‘Enhanced’ object assignment (same key/value shortcut)
+
+// raceResults(['Tom', 'Margaret', 'Allison', 'David', 'Pierre'])
+
+//   {
+//     first: "Tom",
+//     second: "Margaret",
+//     third: "Allison",
+//     rest: ["David", "Pierre"]
+//   }
+
+//////
+// const raceResults = (array) => {
+//   {
+//     first, second, third, [...array];
+//   }
+// };
+
+// One-line Array Swap
+const raceResults = ([first, second, third, ...rest]) => ({first,second,third, rest});
