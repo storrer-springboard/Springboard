e9b70968d2b83aa86fabbbfdf21f308934e9b189
storrer <storrer.chris@gmail.com>
2021-10-10 03:14:53 -0700

    Update list_check.py

--- Python Data Structures Solutions/23_list_check/list_check.py
+++ Python Data Structures Solutions/23_list_check/list_check.py
@@ -13,8 +13,3 @@ def list_check(lst):
             return False
 
     return True
-
-    # Alternate possibilities: use all() with a generator comprehension,
-    # though that isn't something we've covered yet:
-    #
-    # return all(isinstance(item, list) for item in lst)
