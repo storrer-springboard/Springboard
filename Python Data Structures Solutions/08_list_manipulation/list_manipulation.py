b85ce52df9f08c5fb988afffd971ab5948a21e93
storrer <storrer.chris@gmail.com>
2021-10-10 00:13:28 -0700

    Update list_manipulation.py

--- Python Data Structures Solutions/08_list_manipulation/list_manipulation.py
+++ Python Data Structures Solutions/08_list_manipulation/list_manipulation.py
@@ -6,51 +6,27 @@ def list_manipulation(lst, command, location, value=None):
     - location: location to remove/add, either "beginning" or "end"
     - value: when adding, value to add
 
-    remove: remove item at beginning or end, and return item removed
-
-        >>> lst = [1, 2, 3]
-
-        >>> list_manipulation(lst, 'remove', 'end')
-        3
-
-        >>> list_manipulation(lst, 'remove', 'beginning')
-        1
-
-        >>> lst
-        [2]
-
-    add: add item at beginning/end, and return list
-
-        >>> lst = [1, 2, 3]
-
-        >>> list_manipulation(lst, 'add', 'beginning', 20)
-        [20, 1, 2, 3]
-
-        >>> list_manipulation(lst, 'add', 'end', 30)
-        [20, 1, 2, 3, 30]
-
-        >>> lst
-        [20, 1, 2, 3, 30]
-
-    Invalid commands or locations should return None:
-
-        >>> list_manipulation(lst, 'foo', 'end') is None
-        True
-
-        >>> list_manipulation(lst, 'add', 'dunno') is None
-        True
+  
     """
+    # validate input
+    if (command != 'add') and (command != 'remove'):
+        #print("invalid input")
+        return None
+    elif (location != "beginning") and (location != "end"):
+        #print("invalid location")
+        return None
 
-    if command == "remove":
+    # figure out which command to do
+    if command == "add":
         if location == "end":
-            return lst.pop()
-        elif location == "beginning":
-            return lst.pop(0)
-
-    elif command == "add":
-        if location == "beginning":
-            lst.insert(0, value)
-            return lst
-        elif location == "end":
+            #print('adding to the end')
             lst.append(value)
             return lst
+        elif location == "beginning":
+            lst.insert(0,value)
+            return lst
+    if command == "remove":
+        if location == "beginning":
+            return lst.pop(0)
+        elif location == "end":
+            return lst.pop()
