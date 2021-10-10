c82b0f514c0b2ae66ac232732f9dd04590feecf0
storrer <storrer.chris@gmail.com>
2021-10-10 00:37:40 -0700

    Update min_max_key_in_dictionary.py

--- Python Data Structures Solutions/38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
+++ Python Data Structures Solutions/38_min_max_key_in_dictionary/min_max_key_in_dictionary.py
@@ -9,7 +9,7 @@ def min_max_keys(d):
         >>> min_max_keys({"apple": "red", "cherry": "red", "berry": "blue"})
         ('apple', 'cherry')
     """
-
-    keys = d.keys()
-
-    return (min(keys), max(keys))
+    keys = d.keys() # get those keys
+    minim = min(keys) # call the dict method min on them keys
+    maxim = max(keys) # same thing
+    return (minim, maxim) # this casts a tuple
