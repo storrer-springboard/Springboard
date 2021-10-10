a09a1e9fbb797c179ff4a54f498db1a27fb88304
storrer <storrer.chris@gmail.com>
2021-10-10 00:11:52 -0700

    Update capitalize.py

--- Python Data Structures Solutions/13_capitalize/capitalize.py
+++ Python Data Structures Solutions/13_capitalize/capitalize.py
@@ -1,9 +1,5 @@
-def compact(lst):
-    """Return a copy of lst with non-true elements removed.
+def capitalize(phrase):
+    """Capitalize first letter of first word of phrase.
 
     """
-    true_list = []
-    for element in lst:
-        if element:
-            true_list.append(element)
-    return true_list
+    return phrase.capitalize()
