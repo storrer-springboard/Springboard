22badf41e1b753ced9e86ef525272f6259a05af5
storrer <storrer.chris@gmail.com>
2021-10-10 00:14:54 -0700

    Update last_element.py

--- Python Data Structures Solutions/03_last_element/last_element.py
+++ Python Data Structures Solutions/03_last_element/last_element.py
@@ -1,15 +1,8 @@
 def last_element(lst):
     """Return last item in list (None if list is empty.
 
-        >>> last_element([1, 2, 3])
-        3
-
-        >>> last_element([]) is None
-        True
     """
-
-    if lst:
+    if not lst:
+        return None
+    else:
         return lst[-1]
-
-    # we don't need to do anything else; functions
-    # return None by default
