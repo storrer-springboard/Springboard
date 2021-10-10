63686eb250a94aa9294fb7d6904c3e2828e75d06
storrer <storrer.chris@gmail.com>
2021-10-10 00:13:12 -0700

    Update is_palindrome.py

--- Python Data Structures Solutions/09_is_palindrome/is_palindrome.py
+++ Python Data Structures Solutions/09_is_palindrome/is_palindrome.py
@@ -4,23 +4,11 @@ def is_palindrome(phrase):
     Return True/False if phrase is a palindrome (same read backwards and
     forwards).
 
-        >>> is_palindrome('tacocat')
-        True
-
-        >>> is_palindrome('noon')
-        True
-
-        >>> is_palindrome('robert')
-        False
-
-    Should ignore capitalization/spaces when deciding:
-
-        >>> is_palindrome('taco cat')
-        True
-
-        >>> is_palindrome('Noon')
-        True
+      
     """
-
-    normalized = phrase.lower().replace(' ', '')
-    return normalized == normalized[::-1]
+    reverse = phrase[::-1].replace(" ","")
+    no_spaces = phrase.replace(" ","")
+    if reverse.lower() == no_spaces.lower():
+        return True
+    else:
+        return False
