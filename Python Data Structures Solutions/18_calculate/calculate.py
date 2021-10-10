41334e01f9355a8349dc4b82dae5decbe3da9185
storrer <storrer.chris@gmail.com>
2021-10-10 00:09:11 -0700

    Update calculate.py

--- Python Data Structures Solutions/18_calculate/calculate.py
+++ Python Data Structures Solutions/18_calculate/calculate.py
@@ -8,37 +8,25 @@ def calculate(operation, a, b, make_int=False, message='The result is'):
 
     Performs math operation (truncating if make_int), then returns as
     "[message] [result]"
-
-        >>> calculate('add', 2.5, 4)
-        'The result is 6.5'
-
-        >>> calculate('subtract', 4, 1.5, make_int=True)
-        'The result is 2'
-
-        >>> calculate('multiply', 1.5, 2)
-        'The result is 3.0'
-
-        >>> calculate('divide', 10, 4, message='I got')
-        'I got 2.5'
-
-    If a valid operation isn't provided, return None.
-
-        >>> calculate('foo', 2, 3)
-
     """
-
+    if operation != "add" and operation != "subtract" and operation != "multiply" and operation != "divide":
+        return None
+    
     if operation == "add":
-        res = a + b
+        result = a + b
     elif operation == "subtract":
-        res = a - b
+        result = a - b
     elif operation == "multiply":
-        res = a * b
+        result = a * b
     elif operation == "divide":
-        res = a / b
-    else:
-        return
+        result = a / b
+
+
+
 
     if make_int:
-        res = int(res)
-
-    return f"{message} {res}"
+        message += " " + repr(int(result))
+        return message
+    else:
+        message += " " + repr(result)
+        return message
