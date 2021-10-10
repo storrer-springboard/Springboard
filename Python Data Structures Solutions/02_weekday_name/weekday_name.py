2ee16a4aba1bde2e54c71bd953447af3318c24c0
storrer <storrer.chris@gmail.com>
2021-10-10 00:15:01 -0700

    Update weekday_name.py

--- Python Data Structures Solutions/02_weekday_name/weekday_name.py
+++ Python Data Structures Solutions/02_weekday_name/weekday_name.py
@@ -1,29 +1,9 @@
 def weekday_name(day_of_week):
     """Return name of weekday.
 
-        >>> weekday_name(1)
-        'Sunday'
-
-        >>> weekday_name(7)
-        'Saturday'
-
-    For days not between 1 and 7, return None
-
-        >>> weekday_name(9)
-        >>> weekday_name(0)
     """
-
-    DAYS = [
-        "Sunday",
-        "Monday",
-        "Tuesday",
-        "Wednesday",
-        "Thursday",
-        "Friday",
-        "Saturday",
-    ]
-
-    if day_of_week < 1 or day_of_week > 7:
+    days = [None, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
+    if day_of_week > 7 or day_of_week < 1:
         return None
-
-    return DAYS[day_of_week - 1]
+    else:
+        return days[day_of_week]
