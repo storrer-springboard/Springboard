5c917a0ea526caa081b2725f0df0c88b3e485ee1
storrer <storrer.chris@gmail.com>
2021-10-10 01:00:17 -0700

    Update find_the_duplicate.py

--- Python Data Structures Solutions/36_find_the_duplicate/find_the_duplicate.py
+++ Python Data Structures Solutions/36_find_the_duplicate/find_the_duplicate.py
@@ -1,22 +1,17 @@
 def find_the_duplicate(nums):
     """Find duplicate number in nums.
-
     Given a list of nums with, at most, one duplicate, return the duplicate.
     If there is no duplicate, return None
-
         >>> find_the_duplicate([1, 2, 1, 4, 3, 12])
         1
-
         >>> find_the_duplicate([6, 1, 9, 5, 3, 4, 9])
         9
-
         >>> find_the_duplicate([2, 1, 3, 4]) is None
         True
     """
-
-    seen = set()
-
-    for num in nums:
-        if num in seen:
-            return num
-        seen.add(num)
+    unique_numbers = set()
+    for number in nums:
+        if number not in unique_numbers:
+            unique_numbers.add(number)
+        elif number in unique_numbers:
+            return number
