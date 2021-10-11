4280482fa74da450fbb524352915a1524b740d78
storrer <storrer.chris@gmail.com>
2021-10-10 23:33:40 -0700

    Update sum_pairs.py

--- Python Data Structures Solutions/25_sum_pairs/sum_pairs.py
+++ Python Data Structures Solutions/25_sum_pairs/sum_pairs.py
@@ -1,35 +1,20 @@
 def sum_pairs(nums, goal):
     """Return tuple of first pair of nums that sum to goal.
 
-    For example:
 
-        >>> sum_pairs([1, 2, 2, 10], 4)
-        (2, 2)
-
-    (4, 2) sum to 6, and come before (5, 1):
-
-        >>> sum_pairs([4, 2, 10, 5, 1], 6) # (4, 2)
-        (4, 2)
-
-    (4, 3) sum to 7, and finish before (5, 2):
-
-        >>> sum_pairs([5, 1, 4, 8, 3, 2], 7)
-        (4, 3)
-
-    No pairs sum to 100, so return empty tuple:
-
-        >>> sum_pairs([11, 20, 4, 2, 1, 5], 100)
-        ()
     """
+    # set of numbers previously used
+    seen = set()
 
-    already_visited = set()
+    for num2 in nums:
+        # valid numbers add up goal
+        match = goal - num2
 
-    for i in nums:
-        difference = goal - i
-
-        if difference in already_visited:
-            return (difference, i)
-
-        already_visited.add(i)
-
+        # if present valid match number in seen return the tuple 
+        if match in seen:
+            return (match, num2)
+        # else add number to set of seen numbers
+        seen.add(num2) 
+    # if no pair sums to goal then return an empty tuple
     return ()
+sum_pairs([5, 1, 4, 8, 3, 2], 7)
