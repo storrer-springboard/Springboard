871883c861379887cc8fb5472c4547df36f79860
storrer <storrer.chris@gmail.com>
2021-10-10 23:32:49 -0700

    Update titleize.py

--- Python Data Structures Solutions/27_titleize/titleize.py
+++ Python Data Structures Solutions/27_titleize/titleize.py
@@ -7,9 +7,6 @@ def titleize(phrase):
         >>> titleize('oNLy cAPITALIZe fIRSt')
         'Only Capitalize First'
     """
-
-    # there's a built-in method for this!
-    return phrase.title()
-
-    # or, if you didn't know that, could capitalize each word by hand
-    # return ' '.join([s.capitalize() for s in phrase.split(' ')])
+    # using built-in function .title()
+    titleized = phrase.title()
+    return titleized
