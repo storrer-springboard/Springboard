976c2d67bb848dbf6167150f0624df29a0ca219c
storrer <storrer.chris@gmail.com>
2021-10-11 00:03:25 -0700

    Update repeat.py

--- Python Data Structures Solutions/30_repeat/repeat.py
+++ Python Data Structures Solutions/30_repeat/repeat.py
@@ -1,25 +1,11 @@
 def repeat(phrase, num):
     """Return phrase, repeated num times.
 
-        >>> repeat('*', 3)
-        '***'
-
-        >>> repeat('abc', 2)
-        'abcabc'
-
-        >>> repeat('abc', 0)
-        ''
-
-    Ignore illegal values of num and return None:
-
-        >>> repeat('abc', -1) is None
-        True
-
-        >>> repeat('abc', 'nope') is None
-        True
     """
-
-    if not isinstance(num, int) or num < 0:
+    # validate num value    
+    if not isinstance(num, int):
         return None
-
-    return phrase * num
+    if num < 0:
+        return None
+    echo = phrase * num
+    return echo
